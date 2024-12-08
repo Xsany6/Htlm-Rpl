939f016eace07b6f1790c1ce156b26892e8bb7c5
Xsany6 <ff29042028@gmail.com>
2024-12-08 10:58:50 +0700

    Add files via upload

---
+++ video/video js 21-30/javascript-bag27/javascript-bag27/js/json.js
@@ -0,0 +1,22 @@
+let tblmenu = [
+  {
+    "idmenu" : 1,
+    "idkategori" : 1,
+    "menu" : "Apel Manalagi",
+    "harga" : 3000,
+    "warna" : ["merah", "kuning", "hijau"],
+    "stok" : true,
+    "keterangan" : null
+  },
+  {
+    "idmenu" : 2,
+    "idkategori" : 1,
+    "menu" : "Pisang",
+    "harga" : 2000,
+    "warna" : ["merah", "kuning"],
+    "stok" : false,
+    "keterangan" : null
+  }
+]
+
+console.log(tblmenu[0].menu);
